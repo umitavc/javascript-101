b71423b8ba13718ab06f884861cac59f53fa882c
umitavc <umitavc62@gmail.com>
2022-11-07 13:28:23 +0300

    repository is created

---
+++ document-object-model.js
@@ -0,0 +1,7 @@
+
+
+console.log(document.location.pathname)
+
+console.log(document.head)
+console.log(document.body)
+document.body.style.backgroundColor="aqua"
---
+++ number-veri-turu-ile-calismak.js
@@ -0,0 +1,4 @@
+let price =100;
+let tax = 0.18;
+let priceTax = price * tax;
+console.log("fiyat: ",price, "kdv oranı :",tax, "kdv tutari :",priceTax);
