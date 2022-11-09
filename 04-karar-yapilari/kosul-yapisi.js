5c2d461ad6203273936b38eee91d2bcacc614d63
umitavc <umitavc62@gmail.com>
2022-11-09 12:50:23 +0300

    if/else

---
+++ 04-karar-yapilari/kosul-yapisi.js
@@ -0,0 +1,9 @@
+
+
+let username = prompt("kullanıcı adını giriniz :")
+
+if (username.length > 0) {
+    console.log(`Kullanıcı bilginiz : ${username}`)
+} else {
+    console.log(`eksik yada hatalı giriş yaptınız `)
+}
