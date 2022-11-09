9c7c4e62ed99721aa282be90bcdd1f2fad82c69d
umitavc <umitavc62@gmail.com>
2022-11-09 12:28:30 +0300

    added logical operators

---
+++ 04-karar-yapilari/karsilastirma-ve-mantiksal-operatorler.js
@@ -0,0 +1,41 @@
+
+
+let price = "100"
+let user ="umit"
+
+//eşitse
+console.log("== :" ,price == 1)
+console.log("== :" ,price == 100)
+
+//hem dergeri hem turu esitse
+console.log( "== :",price === 1)
+console.log("== :" ,price === 100)
+
+//eşit değilse
+console.log("!= :",user !="guest")
+
+
+//küçükse
+console.log("price <100 :", price <100)
+
+
+//kücük veya eşit
+console.log("price <= 100 :", price <= 100)
+
+
+//büyükse
+console.log("price >1000 :", price >1000)
+
+//büyük veya eşitse
+console.log("price >=99 :", price >=99)
+
+
+//&& ve
+price =0
+console.log( price >0 &&  user!= "guest")
+// || veya
+console.log( price >0 ||  user!= "guest")
+// ! değig (tersi)
+user ="guest"
+price =1
+console.log( price >0 && !user == "guest")
