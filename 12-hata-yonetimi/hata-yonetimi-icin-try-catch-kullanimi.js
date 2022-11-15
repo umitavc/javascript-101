d93b93999d884624a012671c9669d3c30f0fdfc6
umitavc <umitavc62@gmail.com>
2022-11-15 18:50:10 +0300

    try-catch added

---
+++ 12-hata-yonetimi/hata-yonetimi-icin-try-catch-kullanimi.js
@@ -0,0 +1,15 @@
+
+
+let products 
+
+try {
+    products.forEach(item => console.log(item));
+} catch (error) {
+    console.log("error:",error);
+}
+
+
+console.log("hata yonetimi duzgun calısıyor");
+
+let info ="merhaba"
+console.log(info);
